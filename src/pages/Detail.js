78e9e3558f2cffe091c0fd6b67222e62a9ddd25b
byeol yoon <byeol0823@naver.com>
2024-01-01 18:00:58 +0900

    240101

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useContext } from "react-router-dom";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
+
+styled.button(YellowBtn);
+
+let Box = styled.div`
+  background: grey;
+  padding: 20px;
+`;
+
 function Detail(props) {
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(a);
+    };
+  }, []);
+
+  let { id } = useParams();
+
   return (
     <div className="container">
+      {alert == true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -9,9 +50,9 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[0].title}</h4>
-          <p>{props.shoes[0].content}</p>
-          <p>{props.shoes[0].price}</p>
+          <h4 className="pt-5">{props.shoes[id].title}</h4>
+          <p>{props.shoes[id].content}</p>
+          <p>{props.shoes[id].price}</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
